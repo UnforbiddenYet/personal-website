5a8c565273f65805c476da4fa9ae317822de7768
Dmytro <polder-grapnel0e@icloud.com>
2025-10-16 18:49:17 +0200

    feat(home): display taskbar

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,9 +2,13 @@ import usePaintCanvas from "./hooks/useCanvasDraw";
 import photo from './assets/dmytro-pixelated.png'
 import pencil from './assets/pencil.png'
 import spray from './assets/spray.png'
-import eraser from './assets/eraser.png'
+import eraser from './assets/eraser.png';
+import jpegImage from './assets/jpeg-image.ico';
+import myDocuments from './assets/my-documents.ico';
+import textDocument from './assets/text-document.ico';
 
 import './Home.css'
+import { Taskbar } from "./components/Taskbar";
 
 export function Home() {
   const paintCanvasProps = usePaintCanvas({ color: "#000", lineWidth: 4 });
@@ -31,74 +35,82 @@ export function Home() {
     ['spray', spray],
     ['erase', eraser],
   ]
+  const openWindows = [
+    ['About Me', textDocument],
+    ['My Writings', jpegImage],
+    ['Collections', myDocuments]
+  ];
 
   return (
-    <div className="paint-window">
-      <header className="title-bar">
-        <div className="title-text">Dmytro's website - Paint</div>
-        <div className="window-controls">
-          <button aria-label="Minimize">_</button>
-          <button aria-label="Maximize">□</button>
-          <button aria-label="Close">X</button>
-        </div>
-      </header>
-
-      <nav className="menu-bar">
-        <ul>
-          <li><a href="#"><u>F</u>ile</a></li>
-          <li><a href="#"><u>E</u>dit</a></li>
-          <li><a href="#"><u>V</u>iew</a></li>
-          <li><a href="#"><u>H</u>elp</a></li>
-        </ul>
-      </nav>
-
-      <div className="paint-body">
-        <aside className="toolbar">
-          <div className="separator"></div>
-          {tools.map(([tool, imgSrc]) => (
-            <div className={`tool-icon ${paintCanvasProps.isToolActive(tool) ? 'active' : ''}`} onClick={() => paintCanvasProps.setTool(tool)}>
-              <img src={imgSrc} alt={tool} />
-            </div>
-          ))}
-        </aside>
-
-        <main className="canvas" {...{
-          onMouseDown: paintCanvasProps.startDrawing,
-          onMouseUp: paintCanvasProps.finishDrawing,
-          onMouseMove: paintCanvasProps.draw,
-          onMouseLeave: paintCanvasProps.finishDrawing, // Stop drawing if mouse leaves canvas
-        }}>
-          <div className="content-canvas">
-            <h1>HELLO! I'M Dmytro.</h1>
-            <p>I'm awesome human being!</p>
-
-            <div className="pasted-image-container">
-              <img src={photo} alt="A pixelated self portrait of Dmytro." />
-            </div>
-
-            <div>
-              <small>Also, I build amazing apps!</small>
-              <button className="win95-button">
-                View My Work {"->"}
-              </button>
-            </div>
+    <>
+      <div className="paint-window">
+        <header className="title-bar">
+          <div className="title-text">Dmytro's website - Paint</div>
+          <div className="window-controls">
+            <button aria-label="Minimize">_</button>
+            <button aria-label="Maximize">□</button>
+            <button aria-label="Close">X</button>
           </div>
-          <canvas className="paint-canvas" {...{
-            ref: paintCanvasProps.ref,
-          }} />
-        </main>
-      </div>
+        </header>
 
-      <footer className="color-palette">
-        <div className="color-selected">
-          <div className="color-swatch" style={{ backgroundColor: paintCanvasProps.activeColor }}></div>
+        <nav className="menu-bar">
+          <ul>
+            <li><a href="#"><u>F</u>ile</a></li>
+            <li><a href="#"><u>E</u>dit</a></li>
+            <li><a href="#"><u>V</u>iew</a></li>
+            <li><a href="#"><u>H</u>elp</a></li>
+          </ul>
+        </nav>
+
+        <div className="paint-body">
+          <aside className="toolbar">
+            <div className="separator"></div>
+            {tools.map(([tool, imgSrc]) => (
+              <div className={`tool-icon ${paintCanvasProps.isToolActive(tool) ? 'active' : ''}`} onClick={() => paintCanvasProps.setTool(tool)}>
+                <img src={imgSrc} alt={tool} />
+              </div>
+            ))}
+          </aside>
+
+          <main className="canvas" {...{
+            onMouseDown: paintCanvasProps.startDrawing,
+            onMouseUp: paintCanvasProps.finishDrawing,
+            onMouseMove: paintCanvasProps.draw,
+            onMouseLeave: paintCanvasProps.finishDrawing, // Stop drawing if mouse leaves canvas
+          }}>
+            <div className="content-canvas">
+              <h1>HELLO! I'M Dmytro.</h1>
+              <p>I'm awesome human being!</p>
+
+              <div className="pasted-image-container">
+                <img src={photo} alt="A pixelated self portrait of Dmytro." />
+              </div>
+
+              <div>
+                <small>Also, I build amazing apps!</small>
+                <button className="win95-button">
+                  View My Work {"->"}
+                </button>
+              </div>
+            </div>
+            <canvas className="paint-canvas" {...{
+              ref: paintCanvasProps.ref,
+            }} />
+          </main>
         </div>
-        <div className="color-swatches">
-          {colors.map(c => (
-            <button className="color-swatch" style={{ backgroundColor: c }} onClick={() => paintCanvasProps.setColor(c)}></button>
-          ))}
-        </div>
-      </footer>
-    </div>
+
+        <footer className="color-palette">
+          <div className="color-selected">
+            <div className="color-swatch" style={{ backgroundColor: paintCanvasProps.activeColor }}></div>
+          </div>
+          <div className="color-swatches">
+            {colors.map(c => (
+              <button className="color-swatch" style={{ backgroundColor: c }} onClick={() => paintCanvasProps.setColor(c)}></button>
+            ))}
+          </div>
+        </footer>
+      </div>
+      <Taskbar openWindows={openWindows} activeWindow={'About Me'} />
+    </>
   )
 }
