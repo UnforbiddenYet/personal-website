8e3f3e8418158ba5624cfcb5a203f01d2940805d
Dmytro <polder-grapnel0e@icloud.com>
2026-03-08 20:17:47 +0100

    refactor: consolidate global styles into index.css
    Merge App.css into index.css, use CSS variables and mixins for Win95 theme,
    add proper scrollbar styling, and clean up App/main entry files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import './App.css'
-import { Home } from './Home';
-import { WindowManagerProvider } from './contexts';
-import useIsMobile from './hooks/useIsMobile';
-import { NoMobile } from './components/NoMobile';
+import { Home } from "./Home";
+import { WindowManagerProvider } from "./contexts";
+import useIsMobile from "./hooks/useIsMobile";
+import { NoMobile } from "./components/NoMobile";
 
 function App() {
   const { isLoading, isMobile } = useIsMobile();
 
   if (isLoading) return null;
-  if (isMobile) return <NoMobile />
+  if (isMobile) return <NoMobile />;
 
   return (
     <WindowManagerProvider>
-      <div className="app-container">
-        <Home />
-      </div>
+      <Home />
     </WindowManagerProvider>
-  )
+  );
 }
 
-export default App
+export default App;
