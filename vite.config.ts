e3af099a7d61047d0da7ab1a089e286acf7b3301
Dmytro <polder-grapnel0e@icloud.com>
2026-03-09 20:23:00 +0100

    chore: add postcss-nested plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssMixins from 'postcss-mixins'
+import postcssNested from 'postcss-nested'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
       localsConvention: 'camelCaseOnly',
     },
     postcss: {
-      plugins: [postcssMixins({ mixinsFiles: ['src/styles/win95-mixins.css'] })],
+      plugins: [postcssMixins({ mixinsFiles: ['src/styles/win95-mixins.css'] }), postcssNested()],
     },
   },
 })
